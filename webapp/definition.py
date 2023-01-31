e9ff1882b83c208adee3c78ccb49aeb2aba26d8a
thebrightshade <shadmaan@gmail.com>
2023-01-30 21:49:17 -0800

    reformatting

--- webapp/definition.py
+++ webapp/definition.py
@@ -1,7 +1,13 @@
+import pandas as pd
+
 class Definition:
     
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pd.read_csv("data.csv")
+        return tuple(df.loc[df['word']==self.term]['definition'])
+
+d = Definition(term='sun')
+print(d.get())
